28ec1a6faf3ac4bb059dde54abf71fb1e522d368
Walid-Chanane <walid.chanane01@gmail.com>
2025-12-03 15:54:35 +0100

    create base project

---
+++ src/main/java/com/example/pigeonhub/PigeonhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.pigeonhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pigeonhub/PigeonhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pigeonhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
